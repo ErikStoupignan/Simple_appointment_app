ac99b16d49d236de4022c8cc6f0c7e6a80714d20
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2023-03-13 13:08:50 -0600

    Create skeleton for appointments with scaffold command

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_185831) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_190623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.string "start_date"
+    t.string "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
